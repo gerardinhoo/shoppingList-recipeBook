60b0c1a37a8ae6756805bb6c37dfcb353b02dd25
Gerard Eklu <gerard_eklu@homedepot.com>
2021-03-13 16:03:25 -0500

    Allowing the user to add ingredients to the shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,15 +7,19 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
-    new Recipe('Dinho', 'What a baller he was', 'https://cdn.pixabay.com/photo/2016/06/22/18/34/soccer-1473669_1280.jpg')
+    new Recipe('Dinho', 'What a baller he was', 'https://cdn.pixabay.com/photo/2016/06/22/18/34/soccer-1473669_1280.jpg'),
+    new Recipe('Robinho', 'Brazilian', 'https://cdn.pixabay.com/photo/2016/06/22/18/34/soccer-1473669_1280.jpg')
   ];
-
 
   constructor() {
   }
-
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
